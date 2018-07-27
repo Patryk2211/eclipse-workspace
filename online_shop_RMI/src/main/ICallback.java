0e685c4edead9b014214c2499ab63a5820eebc75
Patryk2211 <patrykkluz@wp.pl>
2018-07-27 16:01:14 +0200

    .gitignore

--- online_shop_RMI/src/main/ICallback.java
+++
@@ -1,8 +0,0 @@
-package main;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ICallback extends Remote {
-	public void komunikuj(String text) throws RemoteException;
-}
